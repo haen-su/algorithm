b9351f0942bde4cd7eca0378f315df3a87de02cf
haen-su <haensuya@gmail.com>
2024-08-01 03:05:21 +0900

    [프로그래머스] K번째수

---
+++ programmers/sort/KthNumber.java
@@ -0,0 +1,18 @@
+package programmers.sort;
+
+import java.util.*;
+
+public class KthNumber {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++) {
+            int[] tmp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(tmp);
+            answer[i] = tmp[commands[i][2] - 1];
+        };
+
+        return answer;
+    }
+}
