e74bc4f64ba68ea1f51fa435d8f839a7f4cdb3ff
haen-su <haensuya@gmail.com>
2024-08-01 16:59:50 +0900

    [프로그래머스] H-Index

---
+++ programmers/sort/HIndex.java
@@ -0,0 +1,21 @@
+package programmers.sort;
+
+import java.util.Arrays;
+
+public class HIndex {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for(int h = 0; h <= 10000; h++) {
+            for(int n = 0; n < citations.length; n++) {
+                if ((citations[n] >= h) && (citations.length - n >= h)
+                        && (n <= h) && (answer < h))
+                    answer = h;
+            }
+        }
+
+        return answer;
+    }
+}
