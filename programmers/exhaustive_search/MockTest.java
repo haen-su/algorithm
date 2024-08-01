f92deb41618a571c4634bcbde203bfc59f2e1b38
haen-su <haensuya@gmail.com>
2024-08-02 04:08:52 +0900

    [프로그래머스] 모의고사(시간개선)

--- programmers/exhaustive_search/MockTest.java
+++ programmers/exhaustive_search/MockTest.java
@@ -5,38 +5,28 @@ import java.util.*;
 public class MockTest {
 
     public int[] solution(int[] answers) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int[] scores = {0, 0, 0};
         List<Integer> answer = new ArrayList<>();
 
-        String mark1 = "12345".repeat(2000);
-        String mark2 = "21232425".repeat(1250);
-        String mark3 = "3311224455".repeat(1000);
-
-        String s = "";
-        for(int a : answers) {
-            s += (a + "");
-        }
-
         for(int i = 0; i < answers.length; i++) {
-            if(mark1.charAt(i) == s.charAt(i))
-                scores[0]++;
-            if(mark2.charAt(i) == s.charAt(i))
-                scores[1]++;
-            if(mark3.charAt(i) == s.charAt(i))
-                scores[2]++;
+            if(answers[i] == a[i % a.length]) scores[0]++;
+            if(answers[i] == b[i % b.length]) scores[1]++;
+            if(answers[i] == c[i % c.length]) scores[2]++;
         }
 
-        int max = scores[0];
-        for(int i = 1; i < 2; i++) {
-            if(max < scores[i]) max = scores[i];
-        }
+        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
 
-        for(int i = 0; i < 2; i++) {
-            if(scores[i] == max)
-                answer.add(i + 1);
-        }
+        for(int i = 0; i < 3; i++)
+            if(scores[i] == max) answer.add(i + 1);
 
-        return answer.stream().mapToInt(i->i).toArray();
+        int[] s = new int[answer.size()];
+        int cnt = 0;
+        for(int num : answer)
+            s[cnt++] = num;
+        return s;
     }
 
 }
