c208c067290675c1ef804c52a12a40c274d8de65
haen-su <haensuya@gmail.com>
2024-08-04 16:31:33 +0900

    [프로그래머스] 카펫

---
+++ programmers/exhaustive_search/Carpet.java
@@ -0,0 +1,23 @@
+package programmers.exhaustive_search;
+
+public class Carpet {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        for(int i = 1; i <= yellow; i++) {
+            int y1 = i;
+            int y2 = (brown + 4) / 2 - 4 - y1;
+
+            if(y1 * y2 == yellow) {
+                answer[0] = Math.max(y1, y2) + 2;
+                answer[1] = Math.min(y1, y2) + 2;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+
+
