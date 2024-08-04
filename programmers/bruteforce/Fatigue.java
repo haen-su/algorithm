87e66c2be35f9cd111f230817083ada811c91c74
haen-su <haensuya@gmail.com>
2024-08-05 02:35:49 +0900

    [프로그래머스] 피로도(dfs)

--- programmers/bruteforce/Fatigue.java
+++ programmers/bruteforce/Fatigue.java
@@ -4,40 +4,25 @@ import java.util.*;
 
 public class Fatigue {
     static int max = 0;
+    static boolean[] visited;
 
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
+        visited = new boolean[dungeons.length];
 
-        for(int i = 0; i < dungeons.length; i++) {
-            adventure(k, i, visited.clone(), dungeons);
-        }
+        dfs(k, 0, dungeons);
+
         return max;
     }
 
-    public void adventure(int k, int idx, boolean[] visited, int[][] dungeons) {
-        if(k < dungeons[idx][0] || k == 0) {
-            int cnt = 0;
-            for(boolean b : visited)
-                if (b) cnt++;
-            max = Math.max(max, cnt);
-            return;
-        }
-
-        visited[idx] = true;
-        int cnt = 0;
-
-        for(boolean b : visited)
-            if(b) cnt++;
-
-        if(cnt == visited.length) {
-            max = visited.length;
-            return;
-        }
-
-        for(int i = 0; i < visited.length; i++) {
-            if(!visited[i]) {
-                adventure(k - dungeons[idx][1], i, visited.clone(), dungeons);
+    public void dfs(int k, int cnt, int[][] dungeons) {
+        for(int i = 0; i < dungeons.length; i++) {
+            if(!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], cnt + 1, dungeons);
+                visited[i] = false;
             }
+
+            max = Math.max(max, cnt);
         }
     }
 }
