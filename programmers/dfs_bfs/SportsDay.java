cfce9f17b4df5cbb672d395952ba86835a3b4ebe
haen-su <haensuya@gmail.com>
2024-08-06 23:49:58 +0900

    [프로그래머스] 체육대회

---
+++ programmers/dfs_bfs/SportsDay.java
@@ -0,0 +1,29 @@
+package programmers.dfs_bfs;
+
+public class SportsDay {
+    int max = 0;
+    boolean[] visited;
+
+    public int solution(int[][] ability) {
+        int answer = 0;
+        visited = new boolean[ability.length];
+
+        dfs(ability, 0, 0);
+
+        return max;
+    }
+
+    public void dfs(int[][] ability, int sum, int c) {
+        if(c == ability[0].length) {
+            max = Math.max(max, sum);
+            return;
+        }
+        for(int r = 0; r < ability.length; r++) {
+            if(!visited[r]) {
+                visited[r] = true;
+                dfs(ability, sum + ability[r][c], c + 1);
+                visited[r] = false;
+            }
+        }
+    }
+}
