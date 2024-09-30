55a4e36e58ececb93b3f2dff97a3101ee0e181bd
haen-su <haensuya@gmail.com>
2024-10-01 02:39:16 +0900

    [프로그래머스] 타겟 넘버

--- programmers/dfs_bfs/TargetNumber.java
+++ programmers/dfs_bfs/TargetNumber.java
@@ -1,26 +1,20 @@
 package programmers.dfs_bfs;
 
 public class TargetNumber {
-    static public String[] operations = {"-", "+"};
-    static public int cnt = 0;
+    public int answer = 0;
 
     public int solution(int[] numbers, int target) {
-        operation("", numbers, target);
-        return cnt;
+        dfs(0, 0, numbers, target);
+        return answer;
     }
 
-    public void operation(String sequence, int[] numbers, int target) {
-        if(sequence.length() == numbers.length) {
-            int sum = 0;
-            for(int i = 0; i < numbers.length; i++) {
-                sum = sum + Integer.parseInt(sequence.charAt(i) + Integer.toString(numbers[i]));
-            }
-            if(sum == target) cnt++;
+    public void dfs(int index, int sum, int[] numbers, int target) {
+        if(index == numbers.length) {
+            if(sum == target) answer++;
             return;
         }
 
-        for(int i = 0; i < operations.length; i++) {
-            operation(sequence + operations[i], numbers, target);
-        }
+        dfs(index + 1, sum + numbers[index], numbers, target);
+        dfs(index + 1, sum - numbers[index], numbers, target);
     }
 }
