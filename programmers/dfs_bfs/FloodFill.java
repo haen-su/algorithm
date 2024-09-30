cab1b3fa5ce2f02ebca32ccff5d9657a8e88f1a7
haen-su <haensuya@gmail.com>
2024-10-01 01:48:10 +0900

    BFS_Queue

--- programmers/dfs_bfs/FloodFill.java
+++ programmers/dfs_bfs/FloodFill.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class FloodFill {
     public static void main(String[] args) {
-        int[][] board = {{0, 0, 0, 0, 0},
+        int[][] board = {
+                {0, 0, 0, 0, 0},
                 {0, 0, 0, 1, 1},
                 {0, 0, 0, 1, 0},
                 {1, 1, 1, 1, 0},
