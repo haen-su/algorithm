3432a898c8fd4660f9ffe56f98d97f109600cd21
haen-su <haensuya@gmail.com>
2024-10-02 00:41:25 +0900

    [프로그래머스] 네트워크

--- programmers/dfs_bfs/Network.java
+++ programmers/dfs_bfs/Network.java
@@ -2,34 +2,39 @@ package programmers.dfs_bfs;
 
 import java.util.*;
 public class Network {
-    static public boolean[] visited;
-    static public Queue<Integer> q = new LinkedList<>();
-    static public int cnt = 0;
-
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        int[][] graph = new int[n][n];
+        Stack<Integer> stack = new Stack();
+        boolean[] visited = new boolean[n];
+        int count = 0;
 
         for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                q.offer(i);
-                visited[i] = true;
-                bfs(computers);
-                cnt++;
+            for(int j = 0; j < n; j++) {
+                if(computers[i][j] == 1 || i == j)
+                    graph[i][j] = graph[j][i] = 1;
             }
         }
-        return cnt;
-    }
 
-    public void bfs(int[][] computers) {
-        while(!q.isEmpty()) {
-            int computer = q.poll();
-            for(int i =  0; i < computers.length; i++) {
-                if(!visited[i] && computers[computer][i] == 1) {
-                    visited[i] = true;
-                    System.out.println(i);
-                    q.offer(i);
+        for(int i = 0; i < n; i++) {
+            if(visited[i]) continue;
+
+            stack.push(i);
+
+            while(!stack.empty()) {
+                int current = stack.pop();
+
+                visited[current] = true;
+
+                for(int j = 0; j < n; j++) {
+                    if(j != current && graph[current][j] == 1 && !visited[j])
+                        stack.push(j);
                 }
+
             }
+
+            count++;
         }
+
+        return count;
     }
 }
