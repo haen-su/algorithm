87c7ed2569fb4d2bb77dfb6ee3427d3b18fd586e
haen-su <haensuya@gmail.com>
2024-10-02 04:17:10 +0900

    [프로그래머스] 여행경로

--- programmers/dfs_bfs/TravelCourse.java
+++ programmers/dfs_bfs/TravelCourse.java
@@ -6,54 +6,39 @@ import java.util.*;
 class TravelCourse {
 
     boolean[] visited;
-    List<List<String>> answer = new ArrayList<>();
+    List<String> answer = new ArrayList<>();
 
     public List<String> solution(String[][] tickets) {
         visited = new boolean[tickets.length];
+
         Arrays.sort(tickets, new Comparator<String[]>() {
             @Override
-            public int compare(String[] s1, String[] s2) {
-                if(s1[0].equals(s2[0])) return s1[1].compareTo(s2[1]);
-                return s1[0].compareTo(s2[0]);
+            public int compare(String[] o1, String[] o2) {
+                if(o1[0].equals(o2[0])) return o2[1].compareTo(o1[1]);
+                return o2[0].compareTo(o1[0]);
             }
         });
 
-        List<String> sequence;
-        for(int r = 0; r < tickets.length; r++) {
-            if(tickets[r][0].equals("ICN")) {
-                sequence = new ArrayList<>();
-                sequence.add(tickets[r][0]);
-                sequence.add(tickets[r][1]);
+        List<String> course = new ArrayList<>();
+        course.add("ICN");
 
-                visited[r] = true;
-                dfs(tickets, sequence);
-                visited[r] = false;
-            }
-        }
-
-        return answer.get(0);
+        dfs("ICN", course, tickets);
+        return answer;
     }
 
-    public void dfs(String[][] tickets, List<String> sequence) {
-        boolean finish = true;
-        for(int i = 0; i < visited.length; i++) {
-            if(!visited[i]) {
-                finish = false;
-            }
-        }
-        if(finish) {
-            answer.add(sequence);
+    public void dfs(String current, List<String> course, String[][] tickets) {
+        if(course.size() == tickets.length + 1) {
+            answer = course;
+            return;
         }
 
-        for(int r = 0; r < tickets.length; r++) {
-            if(!visited[r] && tickets[r][0].equals(sequence.get(sequence.size() - 1))) {
-                List<String> tmp = new ArrayList<>();
-                tmp.addAll(sequence);
-                tmp.add(tickets[r][1]);
-
-                visited[r] = true;
-                dfs(tickets, tmp);
-                visited[r] = false;
+        for(int i = 0; i < tickets.length; i++) {
+            if(!visited[i] && current.equals(tickets[i][0])) {
+                visited[i] = true;
+                List<String> tmp = new ArrayList<>(course);
+                tmp.add(tickets[i][1]);
+                dfs(tickets[i][1], tmp, tickets);
+                visited[i] = false;
             }
         }
     }
