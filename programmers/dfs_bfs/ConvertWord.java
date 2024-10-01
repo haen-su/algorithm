04a0657f698925a29869d2328868d815e6174688
haen-su <haensuya@gmail.com>
2024-10-02 01:47:52 +0900

    [프로그래머스] 단어 변환

--- programmers/dfs_bfs/ConvertWord.java
+++ programmers/dfs_bfs/ConvertWord.java
@@ -3,46 +3,46 @@ package programmers.dfs_bfs;
 import java.util.*;
 
 public class ConvertWord {
-    int[] visited;
-    Queue<Integer> q = new LinkedList<>();
-    int answer = 0;
+    static class Node {
+        String word;
+        int dist;
 
-    public int solution(String begin, String target, String[] words) {
-        visited = new int[words.length + 1];
-        String[] tmp = new String[words.length + 1];
-        tmp[0] = begin;
-
-        for(int i = 0; i < words.length; i++)
-            tmp[i + 1] = words[i];
-
-        q.add(0);
-        visited[0] = 0;
-        bfs(tmp, target);
-        return answer;
+        Node(String word, int dist) {
+            this.word = word;
+            this.dist = dist;
+        }
     }
 
-    public void bfs(String[] words, String target) {
-        while(!q.isEmpty()) {
-            String word = words[q.peek()];
-            int idx = q.poll();
+    public int solution(String begin, String target, String[] words) {
+        if(begin.length() != words[0].length()) return 0;
 
-            if(word.equals(target)) {
-                answer = visited[idx];
-                return;
-            }
+        Queue<Node> queue = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
+
+        queue.add(new Node(begin, 0));
+
+        while(!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if(current.word.equals(target))
+                return current.dist;
 
             for(int i = 0; i < words.length; i++) {
-                if(visited[i] == 0) {
-                    int cnt = 0;
-                    for(int j = 0; j < word.length(); j++) {
-                        if(word.charAt(j) == words[i].charAt(j)) cnt++;
-                    }
-                    if(cnt == word.length() - 1) {
-                        q.add(i);
-                        visited[i] = visited[idx] + 1;
-                    }
+                if(visited[i]) continue;
+
+                int count = 0;
+                for(int j = 0; j < begin.length(); j++) {
+                    if(current.word.charAt(j) != words[i].charAt(j))
+                        count++;
+                }
+
+                if(count == 1) {
+                    queue.add(new Node(words[i], current.dist + 1));
+                    visited[i] = true;
                 }
             }
         }
+
+        return 0;
     }
 }
