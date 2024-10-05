c1f0c6e3937574eb42020c832598e56b83ee5218
haen-su <haensuya@gmail.com>
2024-10-05 19:09:14 +0900

    [BOJ] 9251

--- DP/LCS.java
+++ DP/LCS.java
@@ -3,28 +3,32 @@ package DP;
 import java.io.*;
 
 public class LCS {
+    static public Integer[][] dp;
+    static public String s1;
+    static public String s2;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s1 = br.readLine();
-        String s2 = br.readLine();
-
-        System.out.println(lcs(s1, s2));
+        s1 = br.readLine();
+        s2 = br.readLine();
+        dp = new Integer[s1.length() + 1][s2.length() + 1];
+        System.out.println(lcs(s1.length(), s2.length()));
     }
 
-    public static int lcs(String s1, String s2) {
-        int x = s1.length();
-        int y = s2.length();
-
+    public static int lcs(int x, int y) {
         if(x == 0 || y == 0)
             return 0;
 
-        if(s1.charAt(x - 1) == s2.charAt(y - 1))
-            return lcs(s1.substring(0, x - 1), s2.substring(0, y - 1)) + 1;
-        else {
-            return Math.max(lcs(s1.substring(0, x), s2.substring(0, y - 1)),
-                    lcs(s1.substring(0, x - 1), s2.substring(0, y)));
+        if(dp[x][y] == null) {
+            dp[x][y] = 0;
+
+            if(s1.charAt(x - 1) == s2.charAt(y - 1))
+                dp[x][y] = lcs(x - 1, y - 1) + 1;
+            else {
+                dp[x][y] = Math.max(lcs(x - 1, y), lcs(x, y - 1));
+            }
         }
 
+        return dp[x][y];
     }
 }
