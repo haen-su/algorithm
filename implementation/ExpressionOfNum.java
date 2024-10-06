d415b2c17db0637bb61b54afd795896cc1b36e94
haen-su <haensuya@gmail.com>
2024-10-06 09:35:26 +0900

    [프로그래머스] 숫자의 표현

---
+++ implementation/ExpressionOfNum.java
@@ -0,0 +1,21 @@
+package implementation;
+
+public class ExpressionOfNum {
+    public int solution(int n) {
+        int count = 0;
+
+        for(int i = 1; i <= n; i++) {
+            int sum = 0;
+            for(int j = i; j <= n; j++) {
+                sum += j;
+                if(sum == n) {
+                    count++;
+                    break;
+                }
+                if(sum > n) break;
+            }
+        }
+
+        return count;
+    }
+}
